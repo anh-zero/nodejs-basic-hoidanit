6a35ded75b0127ef29e3822f52178c8e93905691
anh-zero <anh.gm.gz@gmail.com>
2024-11-02 22:23:48 +0700

    Vol16: Middleware

--- src/server.js
+++ src/server.js
@@ -3,9 +3,17 @@ import configViewEngine from "./configs/viewEngine";
 import initWebRoute from "./router/web";
 import initAPIRoute from './router/api';
 require("dotenv").config();
-
+var morgan = require('morgan')
 const app = express();
 const post = process.env.PORT || 3000;
+
+app.use((req, res, next) => {
+  //check => return res.send()
+  console.log('>>> run into my middleware')
+  console.log(req.method)
+  next();
+})
+app.use(morgan('combined'))
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -19,6 +27,10 @@ initWebRoute(app);
 // init api route
 initAPIRoute(app);
 
+//handle 404 not found
+app.use((req, res) => {
+  return res.render('404.ejs')
+})
 app.listen(3000, () => {
   console.log(`Example app listen at http://localhost:${post}`);
 });
